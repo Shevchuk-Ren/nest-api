15ab2654ea970bd458a149c06b8ec2593706de63
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-10-20 20:50:30 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.use('/api');
+  app.use('/rest');
   await app.listen(3000 || process.env.PORT);
 }
 bootstrap();
